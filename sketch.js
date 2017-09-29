ce7914925c3dd8c990cd46f4b386dc05c2196c1a
Jason Carter <jsyqrt@gmail.com>
2017-09-29 01:39:11 -0700

    init

--- sketch.js
+++ sketch.js
@@ -1,8 +1,33 @@
+var angle = 0;
+var slider;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);
 }
 
 function draw() {
   background(100);
+  angle = slider.value();
+  stroke(255);
+  translate(200, height);
+  branch(100);
+
+}
+
+
+function branch(len) {
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (len > 4) {
+    push();
+    rotate(angle);
+    branch(len * 0.67);
+    pop();
+
+    push();
+    rotate(-angle);
+    branch(len * 0.67);
+    pop();
+  }
 }
